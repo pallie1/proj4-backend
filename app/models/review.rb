a76be8512c3c2a074c7233f39a1850254231b470
Allie Pacyga <alliep@Allies-MacBook-Air.local>
2020-08-12 19:28:53 -0400

    moved rate from shop model to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :shop
   belongs_to :user
+
+  validates :rate, presence: true
+  validates :content, presence: true
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,6 +3,4 @@ class Shop < ApplicationRecord
     has_many :users, through: :reviews
 
     validates :address, presence: true, uniqueness: true
-    validates :rate, presence: true
-    validates :content, presence: true
 end
