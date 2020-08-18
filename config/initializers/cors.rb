55295d313081cda5d0d9bb7d76a9542be1ef335f
Allie Pacyga <alliep@Allies-MacBook-Air.local>
2020-08-18 14:12:18 -0400

    still working on cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://beanlocal.netlify.app/']
+    origins 'https://beanlocal.netlify.app/'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
+      resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  # end
 end
