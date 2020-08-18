ae81da58aa7ead0dd51ba7461a45edbcd6fe1539
Allie Pacyga <alliep@Allies-MacBook-Air.local>
2020-08-18 14:06:11 -0400

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://beanlocal.netlify.app/', 'http://localhost:3001/']
+    origins ['https://beanlocal.netlify.app/']
 
     resource '*',
       headers: :any,
